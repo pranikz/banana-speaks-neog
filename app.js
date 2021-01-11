a2d6a91c3a85599e6c53e52c1eb9c2686d38de7a
PRATYUSH MAHAPATRA <18btcse42@suiit.ac.in>
2021-01-11 21:13:25 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
  
 btnTranslate.addEventListener("click",function clickEventHandler() {
-    console.log("clicked");
+    outputDiv.innerText = " banana language " + txtInput.value;
+
 })
+
