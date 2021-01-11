f1afc22b476e367b39ae66e53ea7413bdae26cb3
PRATYUSH MAHAPATRA <18btcse42@suiit.ac.in>
2021-01-11 22:41:31 +0530

    Project gone live and banans!

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
- 
-btnTranslate.addEventListener("click",function clickEventHandler() {
-    outputDiv.innerText = " banana language " + txtInput.value;
+
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL= "https://api.funtranslations.com/translate/minion.json"; //api calling 
+
+function getTranslation(text) {
+    return serverURL + "?" + "text=" + text
+}
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something wrong with server Try again after sometime")
+}
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+
+    var inputText = txtInput.value; // taking input
+
+    // calling server for procressing
+    fetch(getTranslation(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; //output
+        })
+        .catch(errorHandler)
 
 })
-
